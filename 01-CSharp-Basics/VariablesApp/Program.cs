63d81c5b13b9a9ca5e214219c05aed321065df0f
Uzair Iqbal <uzair.qq@outlook.com>
2025-07-16 19:28:59 +0500

    Added basic variable declaration examples

---
+++ 01-CSharp-Basics/VariablesApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string fullName = "Uzair Iqbal";
+        int birthYear = 1993;
+        double cgpa = 3.69;
+        bool jobReady = true;
+
+        Console.WriteLine($"Name: {fullName}");
+        Console.WriteLine($"BirthDay: {birthYear}");
+        Console.WriteLine($"CGPA: {cgpa}");
+        Console.WriteLine($"Job Ready?: {jobReady}");
+    }
+}
