3e73b27ce0d2362eb0e30031a836beb50850c688
Uzair Iqbal <uzair.qq@outlook.com>
2025-07-24 19:16:40 +0500

    Deleted Previous✅ Step 1: Variables & Data Types - Basic C# Program

--- 01-CSharp-Basics/VariablesApp/Program.cs
+++ 01-CSharp-Basics/VariablesApp/Program.cs
@@ -4,26 +4,35 @@ class Program
 {
     static void Main()
     {
-        string fullName = "Uzair Iqbal";
-        int birthYear = 1993;
-        double cgpa = 3.69;
-        bool jobReady = true;
+        //Integer Variable
+        int age = 32;
+        Console.WriteLine($"Age: {age}");
 
-        //More Variables
-        char grade = 'A';
-        float height = 5.8f;
-        decimal salary = 450000.75m;
-        bool isOnline = false;
-
-        Console.WriteLine($"Name: {fullName}");
-        Console.WriteLine($"BirthDay: {birthYear}");
-        Console.WriteLine($"CGPA: {cgpa}");
-        Console.WriteLine($"Job Ready?: {jobReady}");
-        Console.WriteLine($"Grade: {grade}");
+        //Float Variable (add f at the end)
+        float height = 5.9f;
         Console.WriteLine($"Height: {height}");
-        Console.WriteLine($"Salary: {salary}");
-        Console.WriteLine($"Online?: {isOnline}");
 
+        //Decimal Variable (add m at the end)
+        decimal salary = 45000.50m;
+        Console.WriteLine($"Salary: {salary}");
+
+        //Character Variable
+        char grade = 'A';
+        Console.WriteLine($"Grade : {grade}");
+
+        //Boolean Variable
+        bool isActive = true;
+        Console.WriteLine($"Is Active: {isActive}");
+
+        //String Variable (Reference Type)
+        string name = "Uzair";
+        Console.WriteLine($"Name: {name}");
+
+        var country = "Pakistan";
+        Console.WriteLine($"Country: {country}");
+
+        const double Pi=3.14159;
+        Console.WriteLine($"Pi: {Pi}");
 
     }
 }
